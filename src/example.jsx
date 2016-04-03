9ddcb5cd87aa901abdb911b1eea963ea6ca2f346
shigemk2 <i.am.shige@gmail.com>
2016-04-03 23:13:25 +0900

    Add react-bootstrap-daterangepicker

--- src/example.jsx
+++ src/example.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
+import DateRangePicker from 'react-bootstrap-daterangepicker';
+import BS from 'react-bootstrap';
+import Button from 'react-bootstrap/lib/Button';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
-const Name = React.createClass({
-    render: function() {
+import cssify from 'cssify';
+
+cssify.byUrl('dist/react-datepicker.min.css');
+
+var HelloWorld = React.createClass({
+    getInitialState: function () {
+		return {
+			ranges: {
+				'Today': [moment(), moment()],
+				'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+				'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+				'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+				'This Month': [moment().startOf('month'), moment().endOf('month')],
+				'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+			},
+			startDate: moment().subtract(29, 'days'),
+			endDate: moment()
+		};
+	},
+	handleEvent: function (event, picker) {
+		this.setState({
+			startDate: picker.startDate,
+			endDate: picker.endDate
+		});
+	},
+    render: function () {
         return (
-            <span>{this.props.name}</span>
+			<DateRangePicker startDate={this.state.startDate} endDate={this.state.endDate} ranges={this.state.ranges} onEvent={this.handleEvent} onApply={this.props.onApply} >
+                <Button style={{width:'220px',fontSize:"12px",height:"32px"}} className="btn-grey btn-ghost">
+                    <div className="pull-left"><Glyphicon glyph="calendar" /></div>
+                </Button>
+            </DateRangePicker>
         );
     }
 });
-const HelloWorld = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <Name name="Test" />
-            </div>
-        );
-    }
-});
+
 ReactDOM.render(
     <HelloWorld />,
     document.getElementById('content')
